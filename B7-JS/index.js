05927865bb59a2a5c5d291d14734bab5081a993f
TungNT <tung@mohasoftware.com>
2022-12-23 21:38:48 +0700

    codition

--- B7-JS/index.js
+++ B7-JS/index.js
@@ -1,49 +1,21 @@
-// Khai báo biến và các kiểu dữ liệu trong js.
-// + Number: 1,2,3,4...
-// + string: text,  '2'
-// + boolean: true/false
-// + object: đối tượng vd student, product, car.
-// vd: {
-//     name: "Tung",
-//     age: 22,
-//     address: "Hà Nội."
-// }
-// + function:
-// function run() {
-//   console.log('tôi đang chạy');
-// }
-// + undefined: value không được assign.
-// JS: ngôn ngữ freestyle=> không cần khai báo kiểu dữ liệu mà JS sẽ tự nhận diện.
-// 3 cách khái báo biên. var, const, let.
-// Khai báo biến. lấy 1 giá trị và lưu vào trong ô nhớ
-// var data = 'đây là giá trị';
-// const data2 = 'Đây là giá trị 2';
-// let data3 = 'đây là giá trị 3';
+// toán tử và câu điều kiện
+// + , - , * , /, % (chia lấy dư) , !(ngược lại) ,       > , < , >=, <=, ==, != (khác), ===, !==
+let a = null; // khai báo 1 biến. nhưng tại thời điểm khai báo ko biết nên gán giá trị gì cho nó
+let b = 3; // number
+let c = '3'; // string
+console.log(b != c); // so sánh cả kiểu dữ liệu
 
-// var data = 10;
-// var a = 20;
-// console.log(data);
-
-// let b = 'Tung';
-// console.log(b);
-// const PI = 3.14; // dùng cho giá trị mà không bao giờ thay đổi.
-// // PI = 5;
-// console.log(PI);
-
-let number = 123;
-let string = 'Tung kjhkjhkhkjhk'; // hiển thị string thuân túy
-let string3 = `Tung ${10 + 20} `; // khi cần xen logic vào string
-let boolean = true;
-let car = {
-  name: 'mada',
-  price: 1000000000000,
-  year: 2022,
-};
-
-function run() {
-  console.log('tôi đang chạy');
+// điều kiện
+let score = 7;
+if (score <= 5) {
+  console.log('bạn bị điểm kém');
+} else if (score > 5 && score < 8) {
+  console.log('bạn được điểm trung bình');
+} else {
+  console.log('bạn điểm cao đấy');
 }
 
-let run2 = () => {};
-
-console.log(typeof tung); // đang sử dụng 1 biến mà không được định nghĩa từ trước.
+let data = null;
+if (data) {
+  console.log('co du lieu');
+}
---
+++ B7-JS/khaibao.js
@@ -0,0 +1,67 @@
+// Khai báo biến và các kiểu dữ liệu trong js.
+// + Number: 1,2,3,4...
+// + string: text,  '2'
+// + boolean: true/false
+// + object: đối tượng vd student, product, car.
+// vd: {
+//     name: "Tung",
+//     age: 22,
+//     address: "Hà Nội."
+// }
+// + function:
+// function run() {
+//   console.log('tôi đang chạy');
+// }
+// + undefined: value không được assign.
+// JS: ngôn ngữ freestyle=> không cần khai báo kiểu dữ liệu mà JS sẽ tự nhận diện.
+// 3 cách khái báo biên. var, const, let.
+// Khai báo biến. lấy 1 giá trị và lưu vào trong ô nhớ
+// var data = 'đây là giá trị';
+// const data2 = 'Đây là giá trị 2';
+// let data3 = 'đây là giá trị 3';
+
+// var data = 10;
+// var a = 20;
+// console.log(data);
+
+// let b = 'Tung';
+// console.log(b);
+// const PI = 3.14; // dùng cho giá trị mà không bao giờ thay đổi.
+// // PI = 5;
+// console.log(PI);
+
+let number = 123;
+let string = 'Tung kjhkjhkhkjhk'; // hiển thị string thuân túy
+let string3 = `Tung ${10 + 20} `; // khi cần xen logic vào string
+let boolean = true;
+let car = {
+  name: 'mada',
+  price: 1000000000000,
+  year: 2022,
+};
+
+function run() {
+  console.log('tôi đang chạy');
+}
+
+let run2 = () => {};
+
+console.log(typeof tung); // đang sử dụng 1 biến mà không được định nghĩa từ trước.
+let _tung = 12;
+const PI = 3.14;
+let nameCar = 'kia k3';
+let numberOfProduct = 10;
+class Bird {}
+// let scopeVar = 10;
+// if (scopeVar == 10) {
+//   let c = 20;
+//   console.log(scopeVar);
+// }
+// console.log(c);
+let x = 10;
+if (x == 10) {
+  let x = 20;
+  console.log(x);
+}
+// scope biến
+
