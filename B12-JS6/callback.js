f8996c988f25845e0d7e954b2156f5a62d592766
Huy V <huy@mohasoftware.com>
2023-02-12 20:30:50 +0700

    demo function

---
+++ B12-JS6/callback.js
@@ -0,0 +1,21 @@
+// C1: callback
+// Bài toán pha cafe: Start => pha cafe trong 5s (doing) => Done.
+const notAsync = () => {
+  setTimeout(() => {
+    console.log('doing');
+  }, 3000);
+};
+
+const doingCoffee = (cb) => {
+  setTimeout(() => {
+    console.log('Doing......');
+    const b = 'Tôi đã có cafe';
+    const c = 'Tôi đã có cafe 2';
+    cb(b, c);
+  }, 1000);
+};
+
+console.log('Start');
+doingCoffee((item, index) => {
+  console.log('Done', item, index);
+});
--- B12-JS6/index.js
+++ B12-JS6/index.js
@@ -1,28 +1,15 @@
-// C1: callback
-// Bài toán pha cafe: Start => pha cafe trong 5s (doing) => Done.
-const notAsync = () => {
-  setTimeout(() => {
-    console.log('doing');
-  }, 3000);
-};
-
-const doingCoffee = (cb) => {
-  setTimeout(() => {
-    console.log('Doing......');
-    const b = 'Tôi đã có cafe';
-    const c = 'Tôi đã có cafe 2';
-    cb(b, c);
-  }, 1000);
-};
-
-console.log('Start');
-doingCoffee((item, index) => {
-  console.log('Done', item, index);
-});
-
 // Tính diện tích hình thang (a + b) * h /2
 // Thực hiện phép cộng mất 3000ms
 // Thực hiện phép nhân mất 2000ms
 // Thực hiện phép chia mất 1000ms
 // Trả về kết quả cuối cùng.
 // Sử dụng callback để xử lý
+
+const tong = (a, b) => {
+  console.log('Start cộng');
+  setTimeout(() => {
+    console.log('Cộng xong', a + b);
+  }, 3000);
+};
+
+tong(5, 6);
