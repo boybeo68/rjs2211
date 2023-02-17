d5b9fcb3d12d4b9e4e0a05ebbf2b116a489ca23e
Huy V <huy@mohasoftware.com>
2023-02-17 20:29:39 +0700

    promise

--- B13-JS7/index.js
+++ B13-JS7/index.js
@@ -22,6 +22,20 @@ const tong = (a, b) => {
     }, 3000);
   });
 };
+const tru = (a, b) => {
+  return new Promise((resolve, reject) => {
+    console.log('Start Tru');
+    setTimeout(() => {
+      const sum = a - b;
+      if (typeof a === 'number' && typeof b === 'number') {
+        console.log('Tru done', sum);
+        resolve(sum);
+      } else {
+        reject('Bạn phải nhập số để thực hiện phép Tru');
+      }
+    }, 500);
+  });
+};
 const nhan = (a, b) => {
   return new Promise((resolve, reject) => {
     console.log('Start Nhan');
@@ -36,33 +50,81 @@ const nhan = (a, b) => {
     }, 2000);
   });
 };
+
 const chia = (a, b) => {
   return new Promise((resolve, reject) => {
     console.log('Start Chia');
     setTimeout(() => {
-      const result = a / b;
-      if (typeof a === 'number' && typeof b === 'number' && b !== 0) {
-        console.log('Chia done', result);
-        resolve(result);
+      if (typeof a != 'number' || typeof b != 'number') {
+        return reject('Bạn phải nhập số');
+      } else if (b == 0) {
+        return reject('Bạn không được phép chia cho 0');
       } else {
-        reject('Bạn phải nhập số để thực hiện phép chia và b != 0');
+        console.log('Chia done', a / b);
+        return resolve(a / b);
       }
-    }, 1000);
+    }, 500);
   });
 };
 
-const ketqua = (a, b, h) => {
-  tong(a, b)
-    .then((result) =>
-      nhan(result, h).then((dataNhan) => {
-        chia(dataNhan, 2).then((ketquaCuoi) => {
-          console.log('kết quả cuối', ketquaCuoi);
-        });
-      }),
-    )
-    .catch((err) => {
-      console.log(err);
+// const ketqua = (a, b, h, g) => {
+//   tong(a, b)
+//     .then((result) =>
+//       nhan(result, h).then((dataNhan) => {
+//         chia(dataNhan, g).then((ketquaCuoi) => {
+//           console.log('kết quả cuối', ketquaCuoi);
+//         });
+//       }),
+//     )
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+const ketqua = (a, b, h, g) => {
+  return tong(a, b)
+    .then((resultTong) => nhan(resultTong, h))
+    .then((ressultNhan) => chia(ressultNhan, g))
+    .then((resultChia) => {
+      console.log('ketqua cuoi', resultChia);
+    })
+    .catch((e) => {
+      console.log('bi loi', e);
     });
 };
 
-ketqua(5, 'tung', 2);
+const ketqua2 = (a, b, h, c, d, e, f) => {
+  return tong(a, b)
+    .then((result) => nhan(result, h))
+    .then((result) => chia((result, c)))
+    .then((result) => nhan((result, d)))
+    .then((result) => tru((result, e)))
+    .then((result) => chia((result, f)))
+    .then((result) => tong((result, c)));
+};
+// Promise chạy tuần tự.
+// Promise chạy song song.
+// Promise chay đua.
+
+// ketqua(5, 7, 6, 2);
+const songsong = () => {
+  return Promise.all([tong(3, 5), nhan(2, 'Tung'), chia(6, 0)])
+    .then((data) => {
+      console.log('ket qua song song', data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const chaydua = () => {
+  return Promise.race([tong(3, 5), nhan('Tung', 3), chia(6, 0)])
+    .then((data) => {
+      console.log('race', data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+// chaydua();
+// songsong();
