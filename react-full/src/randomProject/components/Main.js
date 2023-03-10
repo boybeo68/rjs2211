d69cc1d84a3da1e6bce256b2c094605fd8739113
Huy V <huy@mohasoftware.com>
2023-03-10 22:02:50 +0700

    done basic game

--- react-full/src/randomProject/components/Main.js
+++ react-full/src/randomProject/components/Main.js
@@ -1,27 +1,80 @@
 import React, {useState, useEffect} from 'react';
 import {Container, Row, Button, Form, InputGroup} from 'react-bootstrap';
 import {getRandomInt} from '../../ultils/globalFunction';
+
+const RESULT = {
+  thang: 'thang',
+  thua: 'thua',
+  doing: 'doing',
+};
 const Main = () => {
   const [countGuess, setCountGuess] = useState(0);
   const [randomNumber, setrandomNumber] = useState(null);
   const [inputValue, setinputValue] = useState('');
+  const [result, setResult] = useState('');
+  const [checkResult, setcheckResult] = useState(RESULT.doing);
   const newGame = () => {
     setCountGuess(0);
+    createRandom();
+    setResult('');
+    setcheckResult(RESULT.doing);
   };
   const guess = () => {
+    if (inputValue > randomNumber) {
+      setResult('Số lớn quá rồi');
+    } else if (inputValue < randomNumber) {
+      setResult('Số nhỏ quá rồi');
+    } else {
+      setResult(`Bạn đoán đúng rồi số ngẫu nhiên là ${randomNumber}`);
+      setcheckResult(RESULT.thang);
+    }
     setCountGuess(countGuess + 1);
+    setinputValue('');
   };
 
   useEffect(() => {
-    console.log('mouting');
+    createRandom();
+  }, []);
+
+  useEffect(() => {
+    if (countGuess > 7) {
+      setResult('Bạn đã thua rồi');
+      setcheckResult(RESULT.thua);
+    }
+  }, [countGuess]);
+
+  useEffect(() => {
+    if (checkResult === RESULT.thang) {
+      setResult('Sẽ reset game mới trong 5s...');
+      setTimeout(() => {
+        newGame();
+      }, 5000);
+    }
+  }, [checkResult]);
+
+  const createRandom = () => {
     const random = getRandomInt(1, 100);
     setrandomNumber(random);
-  }, []);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.keyCode === 13) {
+      guess();
+    }
+  };
 
   return (
     <Container>
       <Row>
-        <div className='fs-1 text-center m-5 fw-bold'>
+        <div
+          className={`fs-1 text-center m-5 fw-bold ${
+            checkResult === RESULT.thang
+              ? 'text-success'
+              : checkResult === RESULT.thua
+              ? 'text-danger'
+              : 'text-black'
+          } `}
+        >
           Random Number (1-100)
         </div>
       </Row>
@@ -39,6 +92,7 @@ const Main = () => {
           aria-describedby='basic-addon2'
           type='number'
           value={inputValue}
+          onKeyDown={handleKeyDown}
           onChange={(text) => {
             setinputValue(text.target.value);
           }}
@@ -47,6 +101,7 @@ const Main = () => {
           Guess
         </Button>
       </InputGroup>
+      <p>{result}</p>
     </Container>
   );
 };
