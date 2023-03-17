ecaefc60f15119cbb8cfd279dfc0a8bf3deffc91
Nguyen Thac Tung <tung@mohasoftware.com>
2023-03-17 20:51:07 +0700

    add more api

--- react-full/src/todoApp/index.js
+++ react-full/src/todoApp/index.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 import axios from "axios";
+const URL = "https://6319968e8e51a64d2be76eed.mockapi.io/todos";
 
 export default function Home() {
   const [todos, setTodos] = useState(null);
+  const [error, seterror] = useState(null);
+  const [valueName, setvalueName] = useState("");
+  const [valueDes, setValueDes] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getListTodo();
@@ -10,23 +18,104 @@ export default function Home() {
 
   // get list todo from server
   const getListTodo = async () => {
-    const result = await axios.get(
-      "https://6319968e8e51a64d2be76eed.mockapi.io/todos"
-    );
-    setTodos(result.data);
+    setLoading(true);
+    try {
+      const result = await axios.get(URL);
+      setLoading(false);
+      setTodos(result.data);
+    } catch (error) {
+      setLoading(false);
+      seterror("Có lỗi xảy ra");
+    }
+  };
+
+  const deleteItem = async (id) => {
+    setLoading(true);
+    try {
+      await axios.delete(`${URL}/${id}`);
+      setLoading(false);
+      getListTodo();
+    } catch (error) {
+      setLoading(false);
+      seterror("Có lỗi xảy ra");
+    }
+  };
+
+  const addItem = async () => {
+    try {
+      setLoading(true);
+      await axios.post(URL, {
+        name: valueName,
+        isCheck: false,
+        description: valueDes,
+      });
+      setLoading(false);
+      setvalueName("");
+      setValueDes("");
+      getListTodo();
+    } catch (error) {
+      setLoading(false);
+      seterror("Có lỗi xảy ra");
+    }
   };
 
   return (
     <div>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Nhập name</Form.Label>
+          <Form.Control
+            onChange={(text) => {
+              setvalueName(text.target.value);
+            }}
+            value={valueName}
+            type="text"
+            placeholder="Nhập name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Nhập description</Form.Label>
+          <Form.Control
+            onChange={(text) => {
+              setValueDes(text.target.value);
+            }}
+            value={valueDes}
+            type="text"
+            placeholder="Nhập description"
+          />
+        </Form.Group>
+
+        <Button onClick={addItem} variant="primary">
+          Add
+        </Button>
+      </Form>
+      {loading ? <p>Loading...</p> : null}
       <ul>
         {todos ? (
           todos.map((item, index) => {
-            return <li>{item.name}</li>;
+            return (
+              <li key={index}>
+                <div>
+                  <p>
+                    {item.name} -{" "}
+                    <span
+                      onClick={() => deleteItem(item.id)}
+                      className="deleteData"
+                    >
+                      Delete
+                    </span>
+                  </p>
+                  <i>{item.description}</i>
+                </div>
+              </li>
+            );
           })
         ) : (
           <p>Loading</p>
         )}
       </ul>
+
+      {error && <p>{error}</p>}
     </div>
   );
 }
