919ffd51ffb33832ae49db7a37d733e27ab214f5
Nguyen Thac Tung <tung@mohasoftware.com>
2023-04-02 18:28:41 +0700

    filter data

--- react-full/src/redux/components/Post.js
+++ react-full/src/redux/components/Post.js
@@ -1,37 +1,103 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Card, Button, Row} from 'react-bootstrap';
 import {useSelector, useDispatch} from 'react-redux';
-import {deletePost, filterPost} from '../feature/postSlice';
+import {deletePost, filterPost, resetPost} from '../feature/postSlice';
 
 export default function Post() {
   const posts = useSelector((state) => state.posts);
   const dispatch = useDispatch();
+  const [postLocal, setPostLocal] = useState(null);
   const deleteItem = (id) => {
     dispatch(deletePost(id));
   };
-  const filter = () => {
-    dispatch(filterPost(3));
+
+  useEffect(() => {
+    setPostLocal(posts);
+  }, [posts]);
+  const filter = (rate) => {
+    let converPost = JSON.parse(JSON.stringify(postLocal));
+    converPost = converPost.filter((i) => i.rate > rate);
+    setPostLocal(converPost);
+    // dispatch(filterPost(3));
+  };
+  const filterRedux = (rate) => {
+    dispatch(filterPost(rate));
+  };
+
+  const reset = () => {
+    setPostLocal(posts);
+  };
+  const resetFilter = () => {
+    // setPostLocal(posts);
+    dispatch(resetPost());
   };
 
   return (
     <div>
       <h1>Post list</h1>
-      <Button onClick={filter}>Filter post</Button>
+      <Button onClick={() => filter(3)}>Filter post State</Button>
+      <Button
+        style={{marginLeft: '10px', marginRight: '10px'}}
+        onClick={() => reset()}
+      >
+        reset filter
+      </Button>
+      <Button
+        style={{marginLeft: '10px', marginRight: '10px'}}
+        onClick={() => filterRedux(3)}
+      >
+        Filter redux
+      </Button>
+
+      <Button
+        style={{marginLeft: '10px', marginRight: '10px'}}
+        onClick={() => resetFilter()}
+      >
+        Reset filter
+      </Button>
+
       <Row>
-        {posts.map((item, index) => {
-          return (
-            <Card key={item.id} style={{width: '18rem', marginRight: '10px'}}>
-              <Card.Img variant='top' src='holder.js/100px180' />
-              <Card.Body>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>{item.content}</Card.Text>
-                <Button onClick={() => deleteItem(item.id)} variant='danger'>
-                  Delete
-                </Button>
-              </Card.Body>
-            </Card>
-          );
-        })}
+        {posts.dataFilter
+          ? posts.dataFilter.map((item, index) => {
+              return (
+                <Card
+                  key={item.id}
+                  style={{width: '18rem', marginRight: '10px'}}
+                >
+                  <Card.Img variant='top' src='holder.js/100px180' />
+                  <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>{item.content}</Card.Text>
+                    <Button
+                      onClick={() => deleteItem(item.id)}
+                      variant='danger'
+                    >
+                      Delete
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })
+          : posts.data.map((item, index) => {
+              return (
+                <Card
+                  key={item.id}
+                  style={{width: '18rem', marginRight: '10px'}}
+                >
+                  <Card.Img variant='top' src='holder.js/100px180' />
+                  <Card.Body>
+                    <Card.Title>{item.title}</Card.Title>
+                    <Card.Text>{item.content}</Card.Text>
+                    <Button
+                      onClick={() => deleteItem(item.id)}
+                      variant='danger'
+                    >
+                      Delete
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
       </Row>
     </div>
   );
--- react-full/src/redux/feature/postSlice.js
+++ react-full/src/redux/feature/postSlice.js
@@ -1,23 +1,26 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initialState = [
-  {id: '1', content: 'Nội dung bài viết số 1', title: 'Bài 1', rate: 5},
-  {id: '2', content: 'Hello', title: 'Bài 2', rate: 2},
-  {
-    id: '3',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, earum?',
-    title: 'Bài 3',
-    rate: 3,
-  },
-  {
-    id: '4',
-    content:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore voluptate dicta ab, unde officiis doloribus deleniti veritatis magni similique recusandae.',
-    title: 'Bài 4',
-    rate: 5,
-  },
-];
+const initialState = {
+  data: [
+    {id: '1', content: 'Nội dung bài viết số 1', title: 'Bài 1', rate: 5},
+    {id: '2', content: 'Hello', title: 'Bài 2', rate: 2},
+    {
+      id: '3',
+      content:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, earum?',
+      title: 'Bài 3',
+      rate: 3,
+    },
+    {
+      id: '4',
+      content:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore voluptate dicta ab, unde officiis doloribus deleniti veritatis magni similique recusandae.',
+      title: 'Bài 4',
+      rate: 5,
+    },
+  ],
+  dataFilter: null,
+};
 
 const postSlice = createSlice({
   name: 'post',
@@ -40,12 +43,17 @@ const postSlice = createSlice({
     filterPost: (state, action) => {
       // lọc tất cả những bài post có rate > action.payload
       const converData = JSON.parse(JSON.stringify(state));
-      const DataFilter = converData.filter((i) => i.rate > action.payload);
-      return DataFilter;
+      const DataFilter = converData.data.filter((i) => i.rate > action.payload);
+      converData.dataFilter = DataFilter;
+      return converData;
+    },
+    resetPost: (state, action) => {
+      state.dataFilter = null;
     },
   },
 });
 
-export const {addPost, deletePost, editPost, filterPost} = postSlice.actions;
+export const {addPost, deletePost, editPost, filterPost, resetPost} =
+  postSlice.actions;
 
 export default postSlice.reducer;
